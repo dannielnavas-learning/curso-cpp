d201845951677b5e7b1cb1db176b8e27421a2299
Danniel Navas <dannielnavas@gmail.com>
2026-09-15 07:52:02 -0500

    Agregar archivo principal con mensaje de saludo "Hola mundo"

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+// todo los programas corren desde el main punto de entrada
+int main() {
+    // codigo que se ejecuta al iniciar el programa
+    cout << "Hola mundo";
+}
