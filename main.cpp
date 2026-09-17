e15cd88f9aa5996c08f697e664ae57920ae760f8
Danniel Navas <dannielnavas@gmail.com>
2026-09-17 07:06:37 -0500

    Formatear la función main y agregar comentarios sobre tipos de datos en C++

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 // todo los programas corren desde el main punto de entrada
-int main() {
+int main()
+{
     // codigo que se ejecuta al iniciar el programa
     cout << "Hola mundo";
 }
+
+// Tipo de datos en C++
+
+// bool valor logico true 4 bytes
+// int numero entero 100 4 bytes
+// float numero decimal 3.14 4 bytes
+// double numero decimal de doble precision 3.14159 8 bytes
+// char caracter ASCII 'a' 1 byte
