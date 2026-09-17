d113882a66bee888fc03b50a0d9399a2a10868e0
Danniel Navas <dannielnavas@gmail.com>
2026-09-17 07:12:41 -0500

    Actualizar mensaje de salida en main para mostrar la edad del usuario

--- main.cpp
+++ main.cpp
@@ -5,8 +5,18 @@ using namespace std;
 // todo los programas corren desde el main punto de entrada
 int main()
 {
+    // bool valorLogico = true;
+    // int numeroEntero = 100;
+    // float numeroDecimal = 3.14;
+    // double numeroDecimalDoble = 3.14159;
+    // char caracter = 'a';
+
+    int age_user = 0;
+
+    age_user = 36;
+
     // codigo que se ejecuta al iniciar el programa
-    cout << "Hola mundo";
+    cout << "La edad del usuario es: " << age_user << endl;
 }
 
 // Tipo de datos en C++
