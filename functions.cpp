cfcff4dad46c609daf3b554bce57a29049f88007
Danniel Navas <dannielnavas@gmail.com>
2026-09-23 07:38:16 -0500

    Agregar archivo functions.cpp con una implementación de la función suma

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b);
+
+int main()
+{
+    cout << suma(3, 4) << endl;
+    return 0;
+}
+
+int suma(int a, int b)
+{
+    return a + b;
+}
