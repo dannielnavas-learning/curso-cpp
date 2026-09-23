3c04992a1ad9dd2d3412a1e091795f45f663730b
Danniel Navas <dannielnavas@gmail.com>
2026-09-23 07:15:50 -0500

    Actualizar ejemplo de bucle for para mostrar valores de un arreglo

--- for.cpp
+++ for.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 10; i++)
+    // int limite;
+    int lista[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // cout << "Lista: ";
+    // cin >> limite;
+    int limite = sizeof(lista) / sizeof(lista[0]); // obtener el tamaño del arreglo
+    for (int i = 0; i < limite; i++)
     {
         cout << "Iteración: " << i << endl;
+        cout << "Valor en lista: " << lista[i] << endl;
     }
 
     return 0;
